d19f7c55dccd60b0b4c8d515d11b20bf03734532
Bruno Pergentino <brunovpergentino@gmail.com>
2023-01-30 15:23:40 -0300

    test

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
-// let lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"];
+const controle = document.querySelector("[data-controle]")
 
-// lista = lista.filter(cor => (cor !== "Vermelho") && ( cor !== "Rosa"))
-
-// console.log(lista)
-
-const controle = document.querySelectorAll(".controle-ajuste");
-
-controle.forEach((elemento) => {
-  elemento.addEventListener("click", (evento) => {
-    (manipulaDados(evento.target.textCont, evento.target.parentNode));
-  });
-});
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+    })
+})
 
 function manipulaDados(operacao, controle) {
-  const peca = controle.querySelector(".controle-contador");
-  if (operacao === "-" && parseInt(peca.value) > 0) {
-    peca.value = parseInt(peca.value) - 1;
-  } else if (operacao === "+") {
-    peca.value = parseInt(peca.value) + 1;
-  }
+    const peca = controle.querySelector("[data-contador]")
+
+    if(operacao === "-" && parseInt(peca.value) > 0) {
+        peca.value = parseInt(peca.value) - 1
+    } else if (operacao === "+") {
+        peca.value = parseInt(peca.value) + 1
+    }
 }

2e48b67bcb56b96b1f1f87586c4f359b1ed2ea4f
Bruno Pergentino <brunovpergentino@gmail.com>
2023-01-31 09:14:45 -0300

    Finish Module

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-const controle = document.querySelector("[data-controle]")
+const controle = document.querySelectorAll("[data-controle]")
 
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
 })
 
 function manipulaDados(operacao, controle) {
+    // const reNum = /\d/g
     const peca = controle.querySelector("[data-contador]")
 
-    if(operacao === "-" && parseInt(peca.value) > 0) {
-        peca.value = parseInt(peca.value) - 1
+    if(isNaN(peca.value) === true || peca.value < 0){
+        peca.value = "00"
+    }
+    else if(operacao === "-" && parseInt(peca.value) > 0 ) {
+        if(parseInt(peca.value) === 1){
+            peca.value = "00"
+        } else {
+            peca.value = parseInt(peca.value) - 1
+        }
     } else if (operacao === "+") {
         peca.value = parseInt(peca.value) + 1
+    }
+
+    if (parseInt(peca.value) < 10 && parseInt(peca.value) >= 1){
+        peca.value = "0"+peca.value
     }
 }
